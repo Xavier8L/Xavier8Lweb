878dbdf9ed48746759a0af6133f3aa56a11d46e0
Xavier8L <shenglongchen99@gmail.com>
2024-06-20 02:03:58 +0200

    layout en footer nav gemaakt

---
+++ components/footer.js
@@ -0,0 +1,11 @@
+import Styles from '../styles/footer.module.css';
+
+export default function footer() {
+  return (
+    <footer className={Styles.footer}>
+      <div className={Styles.container}>
+            <p>&copy; 2024 Xavier Chen</p>
+        </div>
+  </footer>
+  );
+}
---
+++ components/navbar.js
@@ -0,0 +1,25 @@
+import Styles from '../styles/navbar.module.css';
+
+export default function navbar() {
+  return (
+    <nav className={Styles.navbar}>
+        <div className={Styles.navbarContainer}> 
+                <a href="#profiel" >
+                    Profiel
+                </a>
+                <a href="#ervaring">
+                    Ervaring
+                </a>
+                <a href="#vaardigheden">
+                    Vaardigheden
+                </a>
+                <a href="#product">
+                    Product
+                </a>
+                <a href="#gegevens">
+                    Persoon gegevens
+                </a>
+        </div>
+    </nav>
+  );
+}

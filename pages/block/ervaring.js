2ad20ef87829107e12e6891b63cac07328e3419e
Xavier8L <shenglongchen99@gmail.com>
2024-11-14 20:31:34 +0100

    ervaring klaar

--- pages/block/ervaring.js
+++ pages/block/ervaring.js
@@ -5,10 +5,23 @@ export default function Ervaring() {
     <div>
       <div className={Styles.container}>
         <div className={Styles.profielTitle}>
-         
-          <div className={Styles.profielText}>adwd</div>
+          Blazter.nl:(ICT bedrijf) September 2020 – Juli 2021
+          <div className={Styles.bold}>Werkzaamheden: </div>
+          <div className={Styles.profielText}>
+            <div>- Maken van Wordpress plugins met behulp van Jquery, Javascript en PHP</div>
+            <div>- Designs omzetten naar websites met Wordpress, CSS, HTML en Javascript</div>
+            <div>- Aanpassen van website functies op basis van klant feedback</div>
+            </div>
         </div>
-        
+        <div className={Styles.profielTitle}>
+          Smart-think (design bedrijf) Juli 2019 – September 2019
+          <div className={Styles.bold}>Werkzaamheden: </div>
+          <div className={Styles.profielText}>
+            <div>- Menu's, logo's, visitekaartjes en spaarpassen ontwerpen met Photoshop.</div>
+            <div>- Designs omzetten naar websites met Wordpress, CSS, HTML en Javascript</div>
+            <div>- Foto maak voor het eten</div>
+          </div>
+        </div>
       </div>
     </div>
   );
